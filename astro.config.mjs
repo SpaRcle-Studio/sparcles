7c065c5c2cb640730e297a2a89197580d6e68690
innerviewer <innerviewerr@gmail.com>
2024-07-11 05:43:25 +0200

    (Site) Added new pages, footer and header.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://sparcles.dev',
+  integrations: [tailwind(), sitemap()],
+  site: 'https://sparcles.dev'
 });
